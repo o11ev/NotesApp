af4bfd065185f1f6aa7ef3ef08023e0aa60f4abb
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2021-02-21 20:45:41 +0300

    add: save new note to array

--- NotesApp/NewNote/NewNoteViewController.swift
+++ NotesApp/NewNote/NewNoteViewController.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class NewNoteViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var titleTextFiled: UITextField!
+    @IBOutlet var noteTextView: UITextView!
+    
+    @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        createNote()
+        dismiss(animated: true)
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        noteTextView.text = "Start text"
+        noteTextView.textColor = UIColor.lightGray
+        titleTextFiled.becomeFirstResponder()
+        
+    }
+}
 
+// MARK: - Adding Note
+extension NewNoteViewController {
+    // Сохранение заметки
+    func createNote() {
+        guard let title = titleTextFiled.text else { return }
+        guard let text = noteTextView.text else { return }
+        
+        // Если заголовок и текст не пустые, то создаем новую заметку и сохраняем
+        if !title.isEmpty && !text.isEmpty {
+            let note = Note(title: title, text: text)
+            DataManager.shared.notes.append(note)
+            DataManager.printNotes()
+            
+        } else {
+            showAlert(with: "Something went wrong",
+                      and: "Please check text of notes")
+        }
+        
+    }
+}
+
+// MARK: - Alert Controller
+extension NewNoteViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        present(alert, animated: true)
+    }
 }
