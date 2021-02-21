156f766cb0085c955d3f064fbd698fe8b1eedcad
mariusaee <mariusae@me.com>
2021-02-21 18:44:59 +0300

    Update NotesListViewController.swift
    Количество ячеек = количеству заметок
    
    Готов промежуточный конфиг ячейки

--- NotesApp/NotesList/NotesListViewController.swift
+++ NotesApp/NotesList/NotesListViewController.swift
@@ -9,38 +9,28 @@ import UIKit
 
 class NotesListViewController: UITableViewController {
     
+    var notes = Note.getNotes()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return notes.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "note", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let note = notes[indexPath.row]
+        
+        content.text = note.title
+        cell.contentConfiguration = content
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
